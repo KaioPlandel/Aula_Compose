7e41c5336224ab9acf1209cd79f3fe0de14f1281
kaioc <kaioplandel@gmail.com>
2022-08-23 21:02:50 -0300

    aula sobre modifiers

--- app/src/main/java/com/plandel/aulacompose01/MainActivity.kt
+++ app/src/main/java/com/plandel/aulacompose01/MainActivity.kt
@@ -1,44 +1,48 @@
 package com.plandel.aulacompose01
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.Text
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.modifier.modifierLocalConsumer
 import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-
             Column(
                 modifier = Modifier
-                    .background(Color.Red)
-                    .fillMaxSize(),
-                horizontalAlignment = Alignment.CenterHorizontally,
-                verticalArrangement = Arrangement.Top
+                    .background(Color.Green)
+                    .fillMaxHeight(0.5f)
+                    .fillMaxWidth()
+                   // .padding(start = 10.dp, top = 18.dp)
+                    .border(5.dp, Color.Magenta)
+                    .padding(10.dp)
+                    .border(5.dp, Color.Red)
+                    .padding(10.dp)
+                    .border(5.dp, Color.Black)
+                    .padding(10.dp)
+                    .border(5.dp, Color.LightGray)
+                  //  .width(600.dp)
+                   // .requiredWidth(600.dp) coloca de fato o tamanho q vc adiciona ele passa a tela do celular
             ) {
-                Row(    //basicamente é um linear layout horizontal e o Column é o vertical
-                    modifier = Modifier // o metodo onde encontra as funções para alterar o layout do item
-                        // .fillMaxSize(0.5f) se deixar sem nada dentro ocupa todo o espaço mas se colocar a fração ocupa o espaco q esta no argumento
-                        .width(200.dp)
-                        .height(300.dp)
-                        //  .fillMaxHeight(0.7f) outra forma de definir o tamanho. aqui diz q vai ocupar 70 por cento da tela do celular
-                        .background(Color.Green),
-                    verticalAlignment = Alignment.CenterVertically, // forma de alinhamento de itens dentro do bloco q esta dentro deste item
-                    horizontalArrangement  = Arrangement.SpaceAround
-                ) {
-                    Text(text = "Hello")
-                    Text(text = "World")
-                    Text(text = "I'm Kaio Plandel!")
-                }
+                Text(text = "Hello", modifier = Modifier.offset(0.dp, 20.dp)) // este item é usado como o margin
+                Spacer(modifier = Modifier.height(50.dp))
+                Text(text = "World", Modifier
+                    .clickable {
+                        Toast.makeText(this@MainActivity,"Hello",Toast.LENGTH_LONG).show()
+                    }
+                )
             }
-
         }
     }
 }
