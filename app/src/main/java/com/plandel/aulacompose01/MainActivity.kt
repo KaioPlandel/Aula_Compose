f2a0a8d23550ef5ec59bc40b056a0a474c838062
kaioc <kaioplandel@gmail.com>
2022-08-23 21:36:30 -0300

    criacao de um card do naruto

--- app/src/main/java/com/plandel/aulacompose01/MainActivity.kt
+++ app/src/main/java/com/plandel/aulacompose01/MainActivity.kt
@@ -1,48 +1,88 @@
 package com.plandel.aulacompose01
 
 import android.os.Bundle
-import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
 import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.modifier.modifierLocalConsumer
+import androidx.compose.ui.graphics.ShaderBrush
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Column(
-                modifier = Modifier
-                    .background(Color.Green)
-                    .fillMaxHeight(0.5f)
-                    .fillMaxWidth()
-                   // .padding(start = 10.dp, top = 18.dp)
-                    .border(5.dp, Color.Magenta)
-                    .padding(10.dp)
-                    .border(5.dp, Color.Red)
-                    .padding(10.dp)
-                    .border(5.dp, Color.Black)
-                    .padding(10.dp)
-                    .border(5.dp, Color.LightGray)
-                  //  .width(600.dp)
-                   // .requiredWidth(600.dp) coloca de fato o tamanho q vc adiciona ele passa a tela do celular
+            val painter = painterResource(id = R.drawable.naruto)
+            val description = "Hokage de Conoha"
+            val title = "Naruto Uzumaki"
+            Box(modifier = Modifier
+                .fillMaxWidth(0.5f)
+                .padding(16.dp)
             ) {
-                Text(text = "Hello", modifier = Modifier.offset(0.dp, 20.dp)) // este item é usado como o margin
-                Spacer(modifier = Modifier.height(50.dp))
-                Text(text = "World", Modifier
-                    .clickable {
-                        Toast.makeText(this@MainActivity,"Hello",Toast.LENGTH_LONG).show()
-                    }
-                )
+                ImageCard(painter = painter,
+                    contentDescription = description,
+                    title = title)
             }
+        }
+    }
+}
+
+@Composable //funções de compose devem iniciar com maiuscula
+fun ImageCard(
+    painter: Painter,
+    contentDescription: String,
+    title: String,
+    modifier: Modifier = Modifier
+) {
+    Card(
+        modifier = Modifier.fillMaxWidth(),
+        shape = RoundedCornerShape(15.dp),
+        elevation = 5.dp
+
+    ) {
+        Box(modifier = Modifier.height(200.dp)) {
+            Image(
+                painter = painter,
+                contentDescription = contentDescription,
+                contentScale = ContentScale.Crop
+            )
+
+            Box(modifier = Modifier
+                .fillMaxSize()
+                .background(
+                    Brush.verticalGradient(
+                        colors = listOf(
+                            Color.Transparent,
+                            Color.Black
+                        ),
+                        startY = 300f
+                    )
+                )
+            )
+
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(12.dp),
+                contentAlignment = Alignment.BottomStart
+            ) {
+                Text(text = title, style = TextStyle(color = Color.White, fontSize = 16.sp))
+            }
+
         }
     }
 }
