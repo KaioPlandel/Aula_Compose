ed0699561e564285336fdf3a0baa1f64bde14966
kaioc <kaioplandel@gmail.com>
2022-08-30 05:56:32 -0300

    Textfields, Buttons & Showing Snackbars

--- app/src/main/java/com/plandel/aulacompose01/MainActivity.kt
+++ app/src/main/java/com/plandel/aulacompose01/MainActivity.kt
@@ -3,32 +3,13 @@ package com.plandel.aulacompose01
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Card
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Brush
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.painter.Painter
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.SpanStyle
-import androidx.compose.ui.text.buildAnnotatedString
-import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import kotlin.random.Random
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
 
@@ -36,101 +17,51 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
 
         setContent {
-            val painter = painterResource(id = R.drawable.naruto)
-            Column(
+
+            val scaffoldState = rememberScaffoldState()
+            var textFieldState by remember {
+                mutableStateOf("")
+            }
+            val scope = rememberCoroutineScope()
+
+            Scaffold(  // Um tipo de layout q trabalha junto, usado como material design componentes
                 modifier = Modifier.fillMaxSize(),
-                horizontalAlignment = Alignment.CenterHorizontally
+                scaffoldState = scaffoldState
             ) {
-                TextTitle(fisrtLetter = "N", text = "aruto")
-                CardImage(
-                    painter = painter,
-                    title = "Naruto",
-                    contentDescription = "Ninja de Konoha"
-                )
-            }
-
-        }
-    }
-
-}
-
-
-@Composable
-fun TextTitle(
-    fisrtLetter: String,
-    text: String
-) {
-    Text(
-        text = buildAnnotatedString {
-            withStyle(
-                style = SpanStyle(
-                    color = Color.Green,
-                    fontSize = 50.sp
-                )
-            ) {
-                append(fisrtLetter)
-            }
-            append(text)
-        })
-}
-
-@Composable
-fun CardImage(
-    painter: Painter,
-    title: String,
-    contentDescription: String
-) {
-    val color = remember {
-        mutableStateOf(Color.Red)
-    }
-    Card(
-        modifier = Modifier
-            .fillMaxSize(0.5f)
-            .padding(10.dp)
-            .clickable {
-                color.value = Color(
-                    Random.nextFloat(),
-                    Random.nextFloat(),
-                    Random.nextFloat(),
-                    1f
-                )
-            },
-        shape = RoundedCornerShape(15.dp),
-        elevation = 6.dp
-    ) {
-        Box(modifier = Modifier.fillMaxSize()) {
-            Image(
-                painter = painter,
-                contentDescription = contentDescription,
-                contentScale = ContentScale.Fit
-            )
-
-
-            Box(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .background(
-                        Brush.verticalGradient(
-                            colors = listOf(
-                                Color.Transparent,
-                                color.value
-                            ), startY = 300f
-                        )
+                Column(
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.Center,
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(horizontal = 30.dp)
+                ) {
+                    TextField(
+                        value = textFieldState,
+                        label = {
+                            Text(text = "Enter your name")
+                        },
+                        onValueChange = {
+                            textFieldState = it
+                        },
+                        singleLine = true,
+                        modifier = Modifier.fillMaxWidth()
                     )
-            )
-
-            Box(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .padding(12.dp),
-                contentAlignment = Alignment.BottomStart
-            ) {
-                Text(text = title, color = Color.Black)
+                    Spacer(modifier = Modifier.height(16.dp))
+                    Button(onClick = {
+                        scope.launch {
+                            scaffoldState.snackbarHostState.showSnackbar("Hello $textFieldState")
+                        }
+                    }) {
+                        Text(text = "Save the Word" )
+                    }
+                }
             }
-        }
 
+        }
     }
+
 }
+
 
 
 
