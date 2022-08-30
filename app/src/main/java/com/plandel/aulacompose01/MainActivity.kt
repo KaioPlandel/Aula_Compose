2a2856bee5c41ffea97803427ae2d977be3bf2e6
kaioc <kaioplandel@gmail.com>
2022-08-29 21:46:47 -0300

    minhas alteracoes

--- app/src/main/java/com/plandel/aulacompose01/MainActivity.kt
+++ app/src/main/java/com/plandel/aulacompose01/MainActivity.kt
@@ -3,16 +3,31 @@ package com.plandel.aulacompose01
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.withStyle
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import kotlin.random.Random
 
 class MainActivity : ComponentActivity() {
@@ -21,56 +36,100 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
 
         setContent {
-
-            Column(modifier = Modifier.fillMaxSize()) {
-                val color = remember {
-                    mutableStateOf(Color.Yellow)
-                }
-
-                ColorBox(
-                    modifier = Modifier
-                        .fillMaxSize()
-                        .weight(1f)
-                ){
-                    color.value = it
-                }
-                Box(
-                    modifier = Modifier
-                        .background(color.value)
-                        .weight(1f)
-                        .fillMaxSize()
-                ) {
-
-                }
+            val painter = painterResource(id = R.drawable.naruto)
+            Column(
+                modifier = Modifier.fillMaxSize(),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                TextTitle(fisrtLetter = "N", text = "aruto")
+                CardImage(
+                    painter = painter,
+                    title = "Naruto",
+                    contentDescription = "Ninja de Konoha"
+                )
             }
-
 
         }
     }
+
+}
+
+
+@Composable
+fun TextTitle(
+    fisrtLetter: String,
+    text: String
+) {
+    Text(
+        text = buildAnnotatedString {
+            withStyle(
+                style = SpanStyle(
+                    color = Color.Green,
+                    fontSize = 50.sp
+                )
+            ) {
+                append(fisrtLetter)
+            }
+            append(text)
+        })
 }
 
 @Composable
-fun ColorBox(
-    modifier: Modifier,
-    updateColor: (Color) -> Unit
+fun CardImage(
+    painter: Painter,
+    title: String,
+    contentDescription: String
 ) {
-    Box(
-        modifier = modifier
-            .background(Color.Red)
+    val color = remember {
+        mutableStateOf(Color.Red)
+    }
+    Card(
+        modifier = Modifier
+            .fillMaxSize(0.5f)
+            .padding(10.dp)
             .clickable {
-                updateColor(
-                    Color(
-                        Random.nextFloat(),
-                        Random.nextFloat(),
-                        Random.nextFloat(),
-                        1f
-                    )
+                color.value = Color(
+                    Random.nextFloat(),
+                    Random.nextFloat(),
+                    Random.nextFloat(),
+                    1f
                 )
-            }
+            },
+        shape = RoundedCornerShape(15.dp),
+        elevation = 6.dp
     ) {
+        Box(modifier = Modifier.fillMaxSize()) {
+            Image(
+                painter = painter,
+                contentDescription = contentDescription,
+                contentScale = ContentScale.Fit
+            )
+
+
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(
+                        Brush.verticalGradient(
+                            colors = listOf(
+                                Color.Transparent,
+                                color.value
+                            ), startY = 300f
+                        )
+                    )
+            )
+
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(12.dp),
+                contentAlignment = Alignment.BottomStart
+            ) {
+                Text(text = title, color = Color.Black)
+            }
+        }
 
     }
-
 }
 
 
