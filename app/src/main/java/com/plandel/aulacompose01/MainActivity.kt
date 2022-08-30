6ddac7d717edd0ff02f8b103564f4be504757f73
kaioc <kaioplandel@gmail.com>
2022-08-30 06:37:00 -0300

    listas

--- app/src/main/java/com/plandel/aulacompose01/MainActivity.kt
+++ app/src/main/java/com/plandel/aulacompose01/MainActivity.kt
@@ -3,13 +3,20 @@ package com.plandel.aulacompose01
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.*
-import androidx.compose.material.*
-import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
-import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
 
@@ -18,46 +25,45 @@ class MainActivity : ComponentActivity() {
 
         setContent {
 
-            val scaffoldState = rememberScaffoldState()
-            var textFieldState by remember {
-                mutableStateOf("")
-            }
-            val scope = rememberCoroutineScope()
+            val narutoImage = painterResource(id = R.drawable.naruto)
 
-            Scaffold(  // Um tipo de layout q trabalha junto, usado como material design componentes
-                modifier = Modifier.fillMaxSize(),
-                scaffoldState = scaffoldState
-            ) {
-                Column(
-                    horizontalAlignment = Alignment.CenterHorizontally,
-                    verticalArrangement = Arrangement.Center,
-                    modifier = Modifier
-                        .fillMaxSize()
-                        .padding(horizontal = 30.dp)
-                ) {
-                    TextField(
-                        value = textFieldState,
-                        label = {
-                            Text(text = "Enter your name")
-                        },
-                        onValueChange = {
-                            textFieldState = it
-                        },
-                        singleLine = true,
-                        modifier = Modifier.fillMaxWidth()
-                    )
-                    Spacer(modifier = Modifier.height(16.dp))
-                    Button(onClick = {
-                        scope.launch {
-                            scaffoldState.snackbarHostState.showSnackbar("Hello $textFieldState")
-                        }
-                    }) {
-                        Text(text = "Save the Word" )
+            // desta forma vc consegue buscar o index da lista
+            LazyColumn {
+                itemsIndexed(
+                    listOf("This", "is", "kaio", "plandel")
+                ) { index, item ->
+                    Column(modifier = Modifier.height(40.dp)) {
+                        CardImage(painter = narutoImage, contentDescription = "Naruto Photo")
                     }
                 }
             }
 
+            LazyColumn {
+                // quantidade de itens da lista
+                items(500) {
+                    CardImage(painter = narutoImage, contentDescription = "Naruto Photo")
+                }
+            }
         }
+    }
+
+}
+
+
+@Composable
+fun CardImage(
+    painter: Painter,
+    contentDescription: String
+) {
+
+    Card(
+        modifier = Modifier
+            .fillMaxSize(0.5f)
+            .padding(10.dp),
+        shape = RoundedCornerShape(15.dp),
+        elevation = 6.dp
+    ) {
+        Image(painter = painter, contentDescription = contentDescription)
     }
 
 }
