a5f1fffe21009ba9408c94d0932f42646ec673f3
kaioc <kaioplandel@gmail.com>
2022-08-31 06:11:20 -0300

    Simple animation

--- app/src/main/java/com/plandel/aulacompose01/MainActivity.kt
+++ app/src/main/java/com/plandel/aulacompose01/MainActivity.kt
@@ -3,20 +3,22 @@ package com.plandel.aulacompose01
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.itemsIndexed
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Card
-import androidx.compose.runtime.Composable
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.Button
+import androidx.compose.material.Text
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.painter.Painter
-import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
+
 
 class MainActivity : ComponentActivity() {
 
@@ -25,23 +27,43 @@ class MainActivity : ComponentActivity() {
 
         setContent {
 
-            val narutoImage = painterResource(id = R.drawable.naruto)
-
-            // desta forma vc consegue buscar o index da lista
-            LazyColumn {
-                itemsIndexed(
-                    listOf("This", "is", "kaio", "plandel")
-                ) { index, item ->
-                    Column(modifier = Modifier.height(40.dp)) {
-                        CardImage(painter = narutoImage, contentDescription = "Naruto Photo")
-                    }
-                }
+            var sizeState by remember {
+                mutableStateOf(200.dp)
             }
 
-            LazyColumn {
-                // quantidade de itens da lista
-                items(500) {
-                    CardImage(painter = narutoImage, contentDescription = "Naruto Photo")
+            val size by animateDpAsState(
+                targetValue = sizeState,
+                tween(
+                    durationMillis = 1000
+                )
+
+//                keyframes {
+//                    durationMillis = 5000
+//                    sizeState at 0 with LinearEasing
+//                    sizeState * 1.5f at 1000 with FastOutLinearInEasing
+//                    sizeState * 2f at 5000
+//                }
+
+//                spring(
+//                    Spring.DampingRatioHighBouncy
+//                )
+//                tween(
+//                    durationMillis = 3000,
+//                    delayMillis = 300,
+//                    easing = LinearOutSlowInEasing
+//                )
+            )
+
+            Box(
+                modifier = Modifier
+                    .size(size)
+                    .background(Color.Red),
+                contentAlignment = Alignment.Center
+            ) {
+                Button(onClick = {
+                    sizeState += 50.dp
+                }) {
+                    Text(text = "Increase Size")
                 }
             }
         }
@@ -49,24 +71,6 @@ class MainActivity : ComponentActivity() {
 
 }
 
-
-@Composable
-fun CardImage(
-    painter: Painter,
-    contentDescription: String
-) {
-
-    Card(
-        modifier = Modifier
-            .fillMaxSize(0.5f)
-            .padding(10.dp),
-        shape = RoundedCornerShape(15.dp),
-        elevation = 6.dp
-    ) {
-        Image(painter = painter, contentDescription = contentDescription)
-    }
-
-}
 
 
 
